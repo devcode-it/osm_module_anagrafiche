3a456f09effdcdd4f9354b3074cec08c36256b61
Maicol Battistini <maicolbattistini@live.it>
2022-03-17 10:55:31 +0100

    fix: Fix temporaneo caricamento nazioni

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,18 +1,19 @@
 <?php /** @noinspection UnusedFunctionResultInspection */
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 use MenaraSolutions\Geographer\Earth;
 
-Route::inertia('anagrafiche', 'openstamanager/anagrafiche::Records', [
-    'nazioni' => array_map(
-        static fn (array $nazione) => [
-            'label' => $nazione['name'],
-            'value' => $nazione['code'],
-        ],
-        (new Earth())
-            ->getCountries()
-            ->setLocale(app()->getLocale())
-            ->toArray()
-    ),
-])
+Inertia::share('nazioni', array_map(
+    static fn (array $nazione) => [
+        'label' => $nazione['name'],
+        'value' => $nazione['code'],
+    ],
+    (new Earth())
+        ->getCountries()
+        ->setLocale(app()->getLocale())
+        ->toArray()
+));
+
+Route::inertia('anagrafiche', 'openstamanager/anagrafiche::Records')
     ->name('anagrafiche');
